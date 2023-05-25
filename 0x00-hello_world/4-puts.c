517f49c8606da2c1dc0749172e66c7b61539158f
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-24 19:09:34 -0700

    Corrected code for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char MyMessage[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyMessage);
-	return(0);
+	return (0);
 }
