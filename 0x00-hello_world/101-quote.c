b033b841e0c5209d1f7ca417e801720d9d5f7ff0
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-25 02:43:29 -0700

    Corrected task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n"\
-			);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	fprintf(stderr, *message);
 	return (1);
 }
