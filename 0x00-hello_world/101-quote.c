6d8f08d46bca26ad2123f81af548a990b92a68bb
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-25 01:06:32 -0700

    Corrected task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,11 @@
  *
  * Description: Print a sentence to the stndrd output
  *
- * return 0 Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+			\n");
 	return (0);
 }
