60c58b50e462b0bae4770cd348f577010ddc560f
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-26 06:39:39 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: This program prints out 0-9 separated by commas
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i++);
+		putchar(',');
+	}
+
+	putchar('\n');
+	return (0);
+}
