13d086e1be6ad8d850bd3756a1985340efbd5205
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-26 06:04:19 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: This program prints out all the lower case alphabet except q and e
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+	char c;
+
+	while (i < 123)
+	{
+		c = i++;
+		if (c != 'e' || c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
