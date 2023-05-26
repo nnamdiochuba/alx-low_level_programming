1ae77d1bfee9110b59df14017e0a31cad4ff0a32
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-26 04:59:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: This program prints out all the lower case alphabet
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+	char c;
+
+	while (i < 123)
+	{
+		c = i;	
+		putchar(c);
+	}
+
+	return (0);
+}
