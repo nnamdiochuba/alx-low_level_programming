24bd4db490ff147218e916c9605a17c91bd1b991
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-27 06:55:26 -0700

    Added file for task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet: Prints lowercse alphabet
+ * Description: Prints lowerase alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return;
+}
