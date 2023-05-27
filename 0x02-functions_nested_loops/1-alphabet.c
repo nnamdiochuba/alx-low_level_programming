c9697d7254d581bbb66b15f94cc4cea63819cc7b
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-27 06:59:18 -0700

    Corrected task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet: Prints lowercse alphabet
+ * print_alphabet- Prints lowercse alphabet
  * Description: Prints lowerase alphabet
  * Return: void
  */
@@ -9,11 +9,11 @@
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i < 123)
 	{
 		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
-	return;
 }
