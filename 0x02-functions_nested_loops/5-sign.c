c46a3e71fe6f8ef78e24883dea4e44968ca0a22b
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-30 05:59:28 -0700

    Task 5 correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,15 +5,22 @@
  * Description: Returns 1 if positive, 0 is zero, and -1 if negative
  * @n: Number input.
  * Return: 1 if greater than 0
- * 	0 if zero
- * 	-1 if less than 0
+ *	0 if zero
+ *	-1 if less than 0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
 	else if (n < 0)
+	{
+		_putchar('-');
 		return (-1);
+	}
+	_putchar('0');
 	return (0);
 }
