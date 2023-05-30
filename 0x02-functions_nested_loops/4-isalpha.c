3a583f7816233c6cb37e6c4753e93abbe6cae8f1
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-30 05:44:11 -0700

    Added file for isalpha task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for lowercase and uppercase alphabet
+ * Description: Returns 1 if a character is alhabet, 0 otherwise
+ * @c: ASCII character input.
+ * Return: 1 or 0 (Sucess)
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	return (0);
+}
