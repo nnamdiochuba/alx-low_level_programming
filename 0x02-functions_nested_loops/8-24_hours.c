4fe9c99f97051ac1cf0f946014576add0e519c2a
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-30 09:01:05 -0700

    Task 8 - Minutes and hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints Jack Bauer's day
+ * Description: Prints every minute of the day from 00:00 to 23:59
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer()
+{
+	int H, h, M, m;
+
+	for (H = 0; H < 3; H++)
+	{
+		for (h = 0; h < 9; h++)
+		{
+			for (M = 0; M < 6; M++)
+			{
+				for (m = 0; m < 9; m++)
+				{
+					if (H == 2 && h > 3)
+						break;
+					_putchar(H + '0');
+					_putchar(h + '0');
+					_putchar(':');
+					_putchar(M + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
