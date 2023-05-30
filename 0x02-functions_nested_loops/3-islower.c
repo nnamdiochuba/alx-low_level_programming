fb74e1679f61cec0e1eaa0b24b263bf67894db4f
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-30 05:32:33 -0700

    Added file for task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercse characters
+ * Description: Returns 1 if a character is lowercase, 0 otherwise
+ * Returns: 1 or 0 (Sucess)
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
