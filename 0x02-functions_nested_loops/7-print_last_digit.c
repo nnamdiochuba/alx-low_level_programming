ff20b45871d1fe6523f8ed98f6ba5668fd4bba3c
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-30 07:49:49 -0700

    Corrected task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@
 int print_last_digit(int num)
 {
 	int last_digit;
-       
+	
 	if (num >= 0)
 		last_digit = num % 10;
 	else
 		last_digit = -1 * (num % 10);
 
-	_putchar(last_digit+'0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
