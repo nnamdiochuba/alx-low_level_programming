2961f15c1429d7ef15b4386a3032fcf2c1354eee
Nnamdi Ochuba <murphyochuba@yahoo.com>
2023-05-31 23:21:19 -0700

    Added file for task 9 - times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the timetable from 0 to 9
+ * Description: Print 9 x 9 timetable starting from 0
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i, j, result, rem;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (result <= 9)
+				_putchar(result + '0');
+			else
+			{
+				rem = result % 10;
+				result = result / 10;
+				_putchar(result + '0');
+				_putchar(rem + '0');
+			}
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
